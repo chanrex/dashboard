c47f3269609c549aa82649081aa9ad9f911e99b2
rubiktwjack <rubiktwjack@gmail.com>
2018-06-19 17:26:16 +0800

    add npm packeg cors

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 var express = require("express");
 var router = express.Router();
 
-const { Pool, Client } = require("pg");
+const {
+  Pool,
+  Client
+} = require("pg");
 
 const client = new Client();
 client.connect();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.render("index", {
     title: "Express"
   });
 });
 
 /* GET Author */
-router.get("/get_author", function(req, res, next) {
+router.get("/get_author", function (req, res, next) {
   client.query("select * from author", (err, result) => {
     console.log(result.rows);
 
     var jsonString = JSON.stringify(result.rows);
-    res.send(jsonString);
-    client.end();
+    res.send(jsonString)
+    //client.end();
   });
+
   /*res.render('index', {
     title: 'Express'
   });*/
