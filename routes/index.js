5491899ddb83a880347fb79a576560d7db8b2764
rubiktwjack <rubiktwjack@gmail.com>
2018-06-21 15:00:00 +0800

    add api

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,71 @@
 var express = require("express");
 var router = express.Router();
 
-const {
-  Pool,
-  Client
-} = require("pg");
+const { Pool, Client } = require("pg");
 
 const client = new Client();
 client.connect();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   res.render("index", {
     title: "Express"
   });
 });
 
 /* GET Author */
-router.get("/get_author", function (req, res, next) {
+router.get("/get_author", function(req, res, next) {
   client.query("select * from author", (err, result) => {
     console.log(result.rows);
 
     var jsonString = JSON.stringify(result.rows);
-    res.send(jsonString)
+    res.send(jsonString);
     //client.end();
   });
+});
 
-  /*res.render('index', {
-    title: 'Express'
-  });*/
+/* GET Book */
+router.get("/get_books", function(req, res, next) {
+  client.query("select * from books", (err, result) => {
+    console.log(result.rows);
+
+    var jsonString = JSON.stringify(result.rows);
+    res.send(jsonString);
+    //client.end();
+  });
+});
+
+/* GET Publisher */
+router.get("/get_publisher", function(req, res, next) {
+  client.query("select * from publisher", (err, result) => {
+    console.log(result.rows);
+
+    var jsonString = JSON.stringify(result.rows);
+    res.send(jsonString);
+    //client.end();
+  });
+});
+
+/* GET Users */
+router.get("/get_users", function(req, res, next) {
+  client.query("select * from users", (err, result) => {
+    console.log(result.rows);
+
+    var jsonString = JSON.stringify(result.rows);
+    res.send(jsonString);
+    //client.end();
+  });
+});
+
+/* GET category */
+router.get("/get_category", function(req, res, next) {
+  client.query("select * from category", (err, result) => {
+    console.log(result.rows);
+
+    var jsonString = JSON.stringify(result.rows);
+    res.send(jsonString);
+    //client.end();
+  });
 });
 
 module.exports = router;
