18d8e92ca0f5e1334b08b7809d7571c9e7d3e96b
rubiktwjack <rubiktwjack@gmail.com>
2018-06-30 20:31:34 +0800

    06/30 add insert api

--- routes/index.js
+++ routes/index.js
@@ -41,13 +41,6 @@ router.post('/insert_author', function (req, res, next) {
       });
     }
   );
-  // client.query("select * from author", (err, result) => {
-  //   console.log(result.rows);
-
-  //   var jsonString = JSON.stringify(result.rows);
-  //   res.send(jsonString);
-  //   //client.end();
-  // });
 });
 
 
@@ -74,6 +67,23 @@ router.get("/get_publisher", function (req, res, next) {
   });
 });
 
+/* Insert Publisher */
+router.post('/insert_publisher', function (req, res, next) {
+  var publisherName = req.body.publisherName;
+
+  client.query("insert into publisher (company_name) values ('" + publisherName + "');",
+    function (error, results, fields) {
+      if (error) throw error;
+      return res.send({
+        data: results,
+        publisherName: publisherName,
+        message: "New task has been created successfully."
+      });
+    }
+  );
+});
+
+
 /* GET Users */
 router.get("/get_users", function (req, res, next) {
   client.query("select * from users", (err, result) => {
@@ -83,6 +93,31 @@ router.get("/get_users", function (req, res, next) {
     res.send(jsonString);
     //client.end();
   });
+});
+
+/* Insert User */
+router.post('/insert_user', function (req, res, next) {
+  var userName = req.body.userName;
+  var userPassword = req.body.userPassword;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var address = req.body.address;
+
+  client.query("insert into users (user_name,user_password,user_email,phone,address) values ('" + userName + "," + userPassword + "," + email + "," + phone + "," + address + "');",
+    function (error, results, fields) {
+      if (error) throw error;
+      return res.send({
+        data: results,
+        userName: userName,
+        userPassword,
+        userPassword,
+        email: email,
+        phone: phone,
+        address: address,
+        message: "New task has been created successfully."
+      });
+    }
+  );
 });
 
 /* GET category */
